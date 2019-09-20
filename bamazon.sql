4f308be51c55a4f7bf75a2880c955123cdfb9a03
Valentyna Kim <tinakim2105@gmail.com>
2019-09-19 18:38:35 -0700

    adding changes

--- bamazon.sql
+++ bamazon.sql
@@ -14,3 +14,13 @@ CREATE TABLE products (
 
 SELECT * FROM products;
 
+INSERT INTO products (product_name,department_name, price, stock_quantity) VALUES ("Plant", "Garden", 7, 80), ("iWatch", "Electronics", 550, 36), 
+("Towell", "Home", 20, 55),
+("FlowerQ", "Beauty", 120, 34),
+("Chair", "Furniture", 55, 12),
+("Cheese", "Food", 8, 45),
+("Visine", "Farmacy", 4, 16),
+("Refrigirator", "Apliances", 150, 32),
+("Logics", "Books", 10, 7),
+("Wheels", "Parts", 200, 15);
+
